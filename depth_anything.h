a22e1a16b3c91e3e6b316198a9a5a2e00b062291
Austin <austin.tran.code@gmail.com>
2025-05-12 14:44:52 -0500

    Update depth_anything.h
    Add grayscale bool

--- depth_anything.h
+++ depth_anything.h
@@ -14,7 +14,7 @@ class DepthAnything
 public:
 	DepthAnything();
     void init(std::string model_path, nvinfer1::ILogger& logger);
-	cv::Mat predict(cv::Mat& image);
+	cv::Mat predict(cv::Mat& image, bool grayscale);
 	~DepthAnything();
 	
 private:
